adc090dc814870a9aecbf49d765d6ac6e603e360
DatSwissGuy <kxosz9@outlook.com>
2020-11-03 16:58:53 +0100

    Add rudimentary route guard for user profile.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CharactersComponent } from './pages/characters/characters.component';
 import { CharacterComponent } from './pages/character/character.component';
 import { LoginComponent } from './components/login/login.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import {ProfileGuardService} from './services/profile-guard.service';
 
 
 const routes: Routes = [
@@ -12,8 +13,8 @@ const routes: Routes = [
   {path: 'character/:id', redirectTo: '/character/:id/comics', pathMatch: 'full'},
   {path: 'character/:id/:tab', component: CharacterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'profile', redirectTo: '/profile/favorites', pathMatch: 'full'},
-  {path: 'profile/:tab', component: UserProfileComponent}
+  {path: 'profile', redirectTo: '/profile/favorites', pathMatch: 'full', canActivate: [ProfileGuardService]},
+  {path: 'profile/:tab', component: UserProfileComponent, canActivate: [ProfileGuardService]}
 ];
 
 @NgModule({
